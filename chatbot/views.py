44eaa37cfbfebb8fc72f94cc420f4ce50d25bf64
ChristopherChantres <chris.chantres@gmail.com>
2023-07-26 19:45:25 -0400

    PulgaB0T renders data properly in the HTML

--- chatbot/views.py
+++ chatbot/views.py
@@ -6,31 +6,17 @@ def home(request):
   context = dict()
 
   if request.method == 'POST':
-      # ------------ NLTK Model ------------#
+    # ------------ NLTK Model ------------#
     chatbot = chatbot_init()
-
     user_input = request.POST['user_field_input']
-
-    #   if user_input.lower() == 'bye':
-    #     print('PulgaB0T: Goodbye!')
-    #     break
-    #   elif user_input.lower() == '--help':
-    #     print("""
-    # - PulgaB0T is a chatbot that uses nltk in the background
-    # - Type for example "Tell me a joke"
-    # - Ask for advices "Give me an advice"
-
-    # Remember that PulgaB0T is still in development, go easy with it! ;)
-              
-    #     """)
     response = chatbot.respond(user_input)
 
     if response == None:
       response = ''
-
     #--------------------------------#
 
     context = {
-      'pulga_response': response
+      'pulga_response': response,
+      'user': user_input
     }
   return render(request, 'chatbot/home.html', context)

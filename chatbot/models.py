a6d51e656e84e0795fb4b31d2f783761badb1c6a
ChristopherChantres <chris.chantres@gmail.com>
2023-07-26 23:36:45 -0400

    Model Message created. User inputs is saved and shown

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+  message = models.TextField(max_length=None, null=None)
+  sent_at = models.DateTimeField(auto_created=True)
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .natural_lang_model.pulga import chatbot_init
+from .models import Message
+from django.utils import timezone
 
 
 def home(request):
@@ -15,8 +17,16 @@ def home(request):
       response = ''
     #--------------------------------#
 
+    # Calling the Model
+    time_sent = timezone.now()
+    message = Message(message=user_input, sent_at=time_sent)
+    message.save()
+
+    all_messages = Message.objects.all()
+
     context = {
       'pulga_response': response,
-      'user': user_input
+      'user': user_input,
+      'all_messages': all_messages,
     }
   return render(request, 'chatbot/home.html', context)

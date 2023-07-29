1fb1dbdbd082f7d5215db9cd240a6294c377aab5
ChristopherChantres <chris.chantres@gmail.com>
2023-07-28 22:41:45 -0400

    Comments added to views.py

--- chatbot/views.py
+++ chatbot/views.py
@@ -7,22 +7,27 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import MessageSerializer
 
+"""
+Home function that holds the user input and outputs
+PulgaB0T response
+"""
 def home(request):
   context = dict()
 
   if request.method == 'POST':
-    # ------------ NLTK Model ------------#
     chatbot = chatbot_init()
     user_input = request.POST['user_field_input']
     user_input = user_input.lower()
     response = chatbot.respond(user_input)
 
+    # Setting response a default value if == none
     if response == None:
       response = 'Remember I am still learning :)'
-    #--------------------------------#
+
+    # Getting the time of the message
+    time_sent = timezone.now()
 
     # Calling the Model
-    time_sent = timezone.now()
     message = Message(message=user_input, sent_at=time_sent)
     message.save()
 
@@ -36,23 +41,22 @@ def home(request):
   return render(request, 'chatbot/home.html', context)
 
 
+# Function that passes all the messages to be displayed
 def messages(request):
   context = dict()
-  counter = 0
 
   if request.method == 'GET':
     all_messages = Message.objects.all()
     context = {
       'all_messages': all_messages,
-      'counter': counter
     }
   return render(request, 'chatbot/messages.html', context)
 
 
+# Functions that serializes the messages to stisfy the API
 @api_view(['GET'])
 def messages_api(request):
   if request.method == 'GET':
     all_messages = Message.objects.all()
     serializer = MessageSerializer(all_messages, many=True)
-
   return Response(serializer.data)
